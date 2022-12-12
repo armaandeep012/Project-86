e9ffcd10d05af3db56e3667de4d8b6023065edcd
A Cat which flies <85874436+armaandeep012@users.noreply.github.com>
2022-12-12 16:46:38 +0530

    Add files via upload

---
+++ Project - 86/Spectagram/config.js
@@ -0,0 +1,12 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIFJdaQTgfCV8M2v1jw1NkbhvYS01Uhz4",
+  authDomain: "storytellingapp-9a836.firebaseapp.com",
+  databaseURL: "https://storytellingapp-9a836-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-9a836",
+  storageBucket: "storytellingapp-9a836.appspot.com",
+  messagingSenderId: "326352563963",
+  appId: "1:326352563963:web:b0016c4b639d242d364a5e",
+  measurementId: "G-R2JB19YN2M"
+};
